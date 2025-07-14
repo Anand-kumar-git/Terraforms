5c0cf83b60e6cbcbe06e1b1b1bc8ee83377ba3b4
Your Name <you@example.com>
2025-07-14 21:04:49 +0530

    subnets added

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "ami-0a1235697f4afa8a4"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-058a493f378e1718b" 
 
   tags = {
     Name = "Jenkins-Terraform-EC2-inatsnce"
