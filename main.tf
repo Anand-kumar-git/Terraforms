f0693deb16e8c206a733cbfa70b77324cf119ffb
Your Name <you@example.com>
2025-07-14 20:43:23 +0530

    New

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"  # or your region
+}
+resource "aws_instance" "ec2" {
+  ami           = "ami-0a1235697f4afa8a4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-EC2"
+  }
+}
