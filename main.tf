e85fc984f18f971247d9b8088a040fd62d926ad3
Your Name <you@example.com>
2025-07-15 18:02:59 +0530

    VPC added

--- main.tf
+++ main.tf
@@ -1,12 +1,29 @@
-provider "aws" {
-  region = "ap-south-1"  # or your region
-}
-resource "aws_instance" "ec2" {
-  ami           = "ami-0a1235697f4afa8a4"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-058a493f378e1718b" 
+pipeline {
+    agent any
 
-  tags = {
-    Name = "Jenkins-Terraform-EC2-inatsnce"
-  }
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anand-kumar-git/Terraforms.git'
+            }
+        }
+
+        stage('Terraform Init') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                sh 'terraform plan'
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                sh 'terraform apply -auto-approve'
+            }
+        }
+    }
 }
